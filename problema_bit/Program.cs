6292ccffb77140cdc92fbf5e753031a86647133a
wesleyornellas <wesleyornellas@gmail.com>
2021-04-03 15:48:29 -0300

    Add projects

---
+++ problema_bit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace problema_bit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notas = {50, 10, 5, 1};
+            int value = Convert.ToInt32(Console.ReadLine());
+            int count = 1;            
+
+            while(value != 0)
+            {
+                Console.WriteLine("Teste {0}",count);                    
+                
+                foreach(int nota in notas){
+                    int quantidade = value/nota;
+                    value = value % nota;
+                    Console.Write(quantidade + " ");                    
+                }                
+                Console.WriteLine("");
+                value = Convert.ToInt32(Console.ReadLine());
+                count++;
+            }
+            
+        }
+    }
+}
---
+++ soma_sequencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace maratona_programacao_alura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                sum += x;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
